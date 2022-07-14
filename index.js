7fc762db923391a353c76b53e0df4016b708b258
Clarky <clarky117@outlook.com>
2022-07-14 11:44:36 +0800

    pseudo code written, inquirer installed

---
+++ index.js
@@ -0,0 +1,41 @@
+// // TODO: Include packages needed for this application
+
+// // TODO: Create an array of questions for user input
+// const questions = [];
+
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
+
+
+
+
+const inquirer = require('inquirer');
+
+// Goal - Generate Markdown Generator to use for future projects
+
+// Ask the user questions;
+
+// What us the Title of Project?
+
+// Sections entitled;
+//  Description,
+//  Table of Contents,
+//  Installation,
+//  Usage,
+//  License,
+//  Contributing,
+//  Tests,
+//  and Questions - is contact info, ask Github and Email
+
+// Auto include Table Of Contents with the above sections
+
+// Add License badge under title of project
+
+// Once all questions are asked we want to generate readme in output folder with user responses
+
